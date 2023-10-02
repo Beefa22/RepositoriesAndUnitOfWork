e0ff3fd2e8e22374ca8ed792add252f3c6167564
YoussefMaklad <118008796+Beefa22@users.noreply.github.com>
2023-10-02 03:48:21 +0300

    add books and Author controller and GetById EndPoint

---
+++ RepositoriesAndUnitOfWork.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepositoriesAndUnitOfWork.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IBaseRepository<Author> _authorRepo;
+
+        public AuthorsController(IBaseRepository<Author> authorRepo)
+        {
+            _authorRepo = authorRepo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthorById(int id)
+        {
+            var author = _authorRepo.GetById(id);
+            return Ok(author);
+        }
+    }
+}
